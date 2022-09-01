f63385cc685bc1600af6c2f6520f437d5f93afe8
Michał Kruszewski <mkru@protonmail.com>
2022-09-01 12:40:41 +0200

    Refactor Simple_PLL

--- simple_pll/src/simple_pll.vhd
+++ simple_pll/src/simple_pll.vhd
@@ -1,62 +1,57 @@
 library ieee;
    use ieee.std_logic_1164.all;
 
--- Simple_PLL is a mock entity for MMCM/PLL with following predefined configuration:
---   1. clocks startup is always safe,
+-- Simple_PLL is a mock entity for MMCM/PLL with following assumptions:
+--   1. input clock is ideal (has not jitter),
+-- and following predefined configuration:
+--   1. output clocks startup is always safe,
 --   2. no dynamic reconfiguration,
 --   3. no reset port as there is no dynamic reconfiguration,
 --   4. duty cycle is always 50 %.
--- Simulating lock loss can be achieved with proper driving of clk_i and INPUT_PERIOD_TOLERANCE generic value.
--- In such case LOCK_LOST_SEVERITY must be set correctly to not cause simulation failure.
+-- Simulating lock loss can be achieved with proper driving of clk_i and LOSS_TIME generic value.
+-- In such case LOSS_SVRITY must be set correctly to not cause simulation failure.
 --
 -- Clocks are specified with periods, not frequencies, because seconds_to_time() function has been introduced in VHDL 2019,
 -- and support for this revision was still poor during development.
 --
--- clk_i must be stable for more than the LOCK_TIME (starting from the rising edge) to allow Simple_PLL to acquire the lock.
+-- IN_PERIOD - input clock period.
+-- LOCK_TIME - period of time for which the input clock must be stable (clk_i period = IN_PERIOD).
+-- LOSS_TIME - period of time in which at least one rising edge of clk_i must appear or Simple_PLL will lose lock.
 --
--- INPUT_PERIOD - input clock period.
+-- CLOCK_COUNT - number of output clocks.
+-- OUT_PERIODS - periods of output clocks.
+-- OUT_PHASES  - phases of output clocks relative to the first rising edge of clk_i after lock.
 --
--- INPUT_PERIOD_TOLERANCE - allowed variation of clk_i period.
--- It must not be set to 0, as the entity will not work correctly.
+-- LOSS_SVRITY - severity of report message when lock is lost.
+-- PREFIX      - optional prefix used in report messages.
 --
--- LOCK_TIME - period of time for which the input clock must be stable (clk_i period <= INPUT_PERIOD + INPUT_PERIOD_TOLERANCE).
---
--- CLOCKS_COUNT - number of output clocks.
---
--- OUTPUT_PERIODS - periods of output clocks.
---
--- OUTPUT_PHASES - phases of output clocks relative to input clock.
---
--- LOCK_LOST_SEVERITY - severity of report message when lock is lost.
---
--- PREFIX - optional prefix used in report messages.
---
--- NOTE: INPUT_PERIOD_TOLERANCE affects only lock mechanism.
--- It does not impact the output frequencies, that are _always_ reciprocal of OUTPUT_PERIODS.
+-- If input clock period differs from IN_PERIOD then warning is reported.
 entity Simple_PLL is
    generic (
       -- Input clock generics.
-      INPUT_PERIOD           : time;
-      INPUT_PERIOD_TOLERANCE : time := 1 fs;
-      LOCK_TIME              : time := 8 * INPUT_PERIOD;
+      IN_PERIOD : time;
+      LOCK_TIME : time := 8 * IN_PERIOD;
+      LOSS_TIME : time := IN_PERIOD + 1 fs;
 
       -- Output clocks generics.
-      CLOCKS_COUNT   : positive := 1;
-      OUTPUT_PERIODS : time_vector(0 to CLOCKS_COUNT - 1);
-      OUTPUT_PHASES  : time_vector(0 to CLOCKS_COUNT - 1) := (others => 0 ns);
+      CLOCK_COUNT : positive := 1;
+      OUT_PERIODS : time_vector(0 to CLOCK_COUNT - 1);
+      OUT_PHASES  : time_vector(0 to CLOCK_COUNT - 1) := (others => 0 ns);
 
-      LOCK_LOST_SEVERITY : severity_level := failure;
-      PREFIX             : string := ""
+      LOSS_SVRITY : severity_level := failure;
+      PREFIX      : string := ""
    );
    port (
       clk_i    : in  std_logic;
-      clks_o   : out std_logic_vector(CLOCKS_COUNT - 1 downto 0);
+      clks_o   : out std_logic_vector(CLOCK_COUNT - 1 downto 0);
       locked_o : out std_logic
    );
 end entity;
 
 
 architecture Behavioral of Simple_PLL is
+
+   constant PERIOD_WAIT_TIME : time := IN_PERIOD + 1 fs;
 
    signal locked : std_logic := '0';
 
@@ -66,29 +61,54 @@ begin
 
 
    input_clk_monitor : process
-      variable prev_clk_rising_edge : time := 0 ns;
-      variable clk_stable_time : time := 0 ns;
+      variable prev_rising_edge : time := 0 ns;
+      variable stable_time : time := 0 ns;
       variable delta : time := 0 ns;
-   begin
-      wait until rising_edge(clk_i) for INPUT_PERIOD + INPUT_PERIOD_TOLERANCE;
 
-      if locked = '1' then
-         if not rising_edge(clk_i) then
+      variable wait_time : time := PERIOD_WAIT_TIME;
+
+      procedure check_period is begin
+         -- Ignore first rising edge during simulation startup or after lock loss.
+         if prev_rising_edge = 0 fs then
+            return;
+         end if;
+
+         if delta /= IN_PERIOD then
+            report PREFIX & "clk_i period equals " & to_string(delta) &
+               ", expecting " & to_string(IN_PERIOD)
+               severity warning;
+         end if;
+      end procedure;
+
+   begin
+      wait until rising_edge(clk_i) for wait_time;
+
+      if locked then
+         if rising_edge(clk_i) then
+            delta := now - prev_rising_edge;
+            check_period;
+            prev_rising_edge := now;
+         else
             locked <= '0';
-            report PREFIX & "lock lost" severity LOCK_LOST_SEVERITY;
+            wait_time := PERIOD_WAIT_TIME;
+            prev_rising_edge := 0 ns;
+            report PREFIX & "lock lost, no clk_i rising edge for " & to_string(LOSS_TIME) severity LOSS_SVRITY;
          end if;
       else
          if rising_edge(clk_i) then
-            delta := now - prev_clk_rising_edge;
-            if delta <= INPUT_PERIOD + INPUT_PERIOD_TOLERANCE then
-               clk_stable_time := clk_stable_time + delta;
-            else
-               clk_stable_time := 0 ns;
-            end if;
-            prev_clk_rising_edge := now;
+            delta := now - prev_rising_edge;
+            check_period;
+            prev_rising_edge := now;
 
-            if clk_stable_time > LOCK_TIME then
+            if delta = IN_PERIOD then
+               stable_time := stable_time + delta;
+            else
+               stable_time := 0 ns;
+            end if;
+
+            if stable_time > LOCK_TIME then
                locked <= '1';
+               wait_time := LOSS_TIME;
                report PREFIX & "lock acquired";
             end if;
          end if;
@@ -96,20 +116,18 @@ begin
    end process;
 
 
-   output_clocks : for o in 0 to CLOCKS_COUNT - 1 generate
+   output_clocks : for o in 0 to CLOCK_COUNT - 1 generate
 
       process is
       begin
+         wait until locked;
+         wait for OUT_PHASES(o);
          loop
-            wait until locked;
-            wait for OUTPUT_PHASES(o);
-            loop
-               clks_o(o) <= '1';
-               wait for OUTPUT_PERIODS(o) / 2;
-               clks_o(o) <= '0';
-               wait for OUTPUT_PERIODS(o) / 2;
-               exit when not locked;
-            end loop;
+            clks_o(o) <= '1';
+            wait for OUT_PERIODS(o) / 2;
+            clks_o(o) <= '0';
+            wait for OUT_PERIODS(o) / 2;
+            exit when not locked;
          end loop;
       end process;
 
--- simple_pll/tb/tb.vhd
+++ simple_pll/tb/tb.vhd
@@ -10,7 +10,7 @@ end entity;
 
 architecture test of tb_simple_pll is
 
-   constant INPUT_CLK_PERIOD : time := 10 ns;
+   constant IN_CLK_PERIOD : time := 10 ns;
    signal in_clk : std_logic := '0';
 
    signal out_clk_0, out_clk_1 : std_logic;
@@ -22,28 +22,29 @@ begin
    begin
       for i in 0 to 20 loop
          in_clk <= '1';
-         wait for INPUT_CLK_PERIOD / 2;
+         wait for IN_CLK_PERIOD / 2;
          in_clk <= '0';
-         wait for INPUT_CLK_PERIOD / 2;
+         wait for IN_CLK_PERIOD / 2;
       end loop;
       wait for 100 ns;
       for i in 0 to 20 loop
          in_clk <= '1';
-         wait for INPUT_CLK_PERIOD / 2;
+         wait for IN_CLK_PERIOD / 2;
          in_clk <= '0';
-         wait for INPUT_CLK_PERIOD / 2;
+         wait for IN_CLK_PERIOD / 2;
       end loop;
    end process;
 
 
    DUT : entity sim.Simple_PLL
    generic map (
-      INPUT_PERIOD   => INPUT_CLK_PERIOD,
-      LOCK_TIME      => 40 ns,
-      CLOCKS_COUNT   => 2,
-      OUTPUT_PERIODS => (INPUT_CLK_PERIOD, 4 * INPUT_CLK_PERIOD),
-      OUTPUT_PHASES  => (2.5 ns, 5 ns),
-      LOCK_LOST_SEVERITY => WARNING
+      IN_PERIOD   => IN_CLK_PERIOD,
+      LOCK_TIME   => 40 ns,
+      CLOCK_COUNT => 2,
+      OUT_PERIODS => (IN_CLK_PERIOD, 4 * IN_CLK_PERIOD),
+      OUT_PHASES  => (2.5 ns, 5 ns),
+      LOSS_SVRITY => ERROR,
+      PREFIX      => "sim PLL: "
    ) port map (
       clk_i     => in_clk,
       clks_o(0) => out_clk_0,
@@ -54,7 +55,7 @@ begin
 
    main : process is
    begin
-      wait for 50 * INPUT_CLK_PERIOD;
+      wait for 50 * IN_CLK_PERIOD;
       std.env.finish;
    end process;
 
